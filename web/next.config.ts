74205974af28b0d95df97cf96121a2a823735c32
me-lucas-al <me.lucasalmeida@gmail.com>
2026-02-18 14:47:59 -0300

    fix: estabilashed prisma versions and tsconfig bugs

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@portfolio/core', '@portfolio/packages'],
+  transpilePackages: ['@portfolio/core', '@portfolio/packages', '@portfolio/database'],
 };
 
 export default nextConfig;
