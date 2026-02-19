5c7c074cd646d4b9aaa142fa358140f83c32b526
me-lucas-al <me.lucasalmeida@gmail.com>
2026-02-19 15:26:31 -0300

    feat: initialize turborepo

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { authConfig } from "./auth.config"
-import { AuthService }  from "@core/src/services/_auth" 
-import { CreateAdminType } from "@packages/index"
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "./auth.config";
+import { AuthService } from "@portfolio/core/src/services/_auth";
+import { CreateAdminType } from "@portfolio/packages/index";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
@@ -15,19 +15,19 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       },
       authorize: async (credentials) => {
         try {
-          const user = await AuthService.login(credentials as CreateAdminType)
-          
+          const user = await AuthService.login(credentials as CreateAdminType);
+
           if (user) {
             return {
               ...user,
               id: user.id.toString(),
-            }
+            };
           }
-          return null
+          return null;
         } catch (error) {
-          return null
+          return null;
         }
       },
     }),
   ],
-})
+});
