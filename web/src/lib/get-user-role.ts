979f168f7b3e35dbb80f029ff8c58e094447d530
me-lucas-al <me.lucasalmeida@gmail.com>
2026-02-26 15:27:05 -0300

    refactor: remove redirect logic from getUserRole and useSession hook

--- web/src/lib/get-user-role.ts
+++ web/src/lib/get-user-role.ts
@@ -1,9 +1,7 @@
 import { auth } from "@/auth";
-import { redirect } from "next/navigation";
 
 export async function getUserRole(userRole: Roles) {
-    const session = await auth();
-    if (!session) redirect('/login'); 
+    const session = await auth(); 
     
     const userRoleFromSession = session?.user?.role;
     return userRoleFromSession === userRole;
--- web/src/lib/use-get-user-role.ts
+++ web/src/lib/use-get-user-role.ts
@@ -1,19 +1,9 @@
 // src/lib/use-get-user-role.ts
 'use client';
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export function useGetUserRole(userRole: Roles) {
     const { data: session, status } = useSession(); 
-    
-    const router = useRouter();
-
-    useEffect(() => {
-        if (status === "unauthenticated") {
-            router.push('/login');
-        }
-    }, [status, router]);
 
     if (status === "loading" || !session) {
         return false;
