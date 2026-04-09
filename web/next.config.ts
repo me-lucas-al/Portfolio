5f266132565bf1e5ebd6d2e0ef1bf7cefc62b5a3
me-lucas-al <lucasalsouza2006@gmail.com>
2026-04-09 20:27:04 -0300

    feat: remove distdir

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@portfolio/core', '@portfolio/packages', '@portfolio/database'],
-  distDir: 'dist',
   experimental: {
     serverActions: {
       bodySizeLimit: '20mb',
